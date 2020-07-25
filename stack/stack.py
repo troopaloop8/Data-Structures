90d1a71b88927c50f6ea4a01b046a60c637578b7
troopaloop8 <jontroop@gmail.com>
2020-07-25 11:49:21 +0900

    passing tests for singly linked lists, stack, and queue

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,21 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+import sys
+sys.path.append('..')
+from singly_linked_list.singly_linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        return self.storage.append(value)
 
     def pop(self):
-        pass
+        return self.storage.pop()
